13fee5f608a1dc6c19f6242a0ef5665b00fc060c
Reda Benchraa <redaBenchraa@gmail.com>
2019-02-12 13:42:19 +0100

    detail activity

--- app/src/main/java/com/example/nowintheather/DetailActivity.java
+++ app/src/main/java/com/example/nowintheather/DetailActivity.java
@@ -1,14 +1,42 @@
 package com.example.nowintheather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatRatingBar;
+import butterknife.BindView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.example.nowintheather.Models.MovieShowTime;
 
 public class DetailActivity extends AppCompatActivity {
+    @BindView(R.id.title)
+    TextView title;
+
+    @BindView(R.id.description)
+    TextView description;
+
+    @BindView(R.id.video)
+    VideoView video;
+
+    @BindView(R.id.audianceRating)
+    AppCompatRatingBar audianceRating;
+
+    @BindView(R.id.pressRating)
+    AppCompatRatingBar pressRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Intent intent = getIntent();
+        MovieShowTime movie = (MovieShowTime) intent.getSerializableExtra("movie");
+        title.setText(movie.getOnShow().getMovie().getTitle());
+        title.setText(movie.getOnShow().getMovie().getTitle());
+        pressRating.setRating(Float.parseFloat(movie.getOnShow().getMovie().getStatistics().getPressRating()));
+        audianceRating.setRating(Float.parseFloat(movie.getOnShow().getMovie().getStatistics().getUserRating()));
+        video.setVideoPath(movie.getOnShow().getMovie().getTrailer().getHref());
     }
 }
